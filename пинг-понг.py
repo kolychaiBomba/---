e9eeed561ec2ac367388c464ffaa3f6179a23a7b
Belash <Belash1973192010@gmail.com>
2024-05-16 19:30:28 +0300

    v1.3

--- пинг-понг.py
+++ пинг-понг.py
@@ -1,6 +1,6 @@
 from pygame import *
 
-#todo окно
+#todo. окно
 window = display.set_mode((1000, 700))
 display.set_caption('Пинг-понг')
 
@@ -14,7 +14,7 @@ FPS = 70
 #mixer.music.play()
 
 font.init()
-font1 = font.SysFont('microsoftjhengheimicrosoftjhengheiuilight', 36)
+font2 = font.SysFont('tahoma', 50)
 #print(font.get_fonts())
 
 #! класс игрока
@@ -31,20 +31,12 @@ class qwerSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
     def update(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_a] and self.rect.x > 0:
-            self.rect.x -= 5    
-        if keys_pressed[K_d] and self.rect.x < 950:
-            self.rect.x += 5
         if keys_pressed[K_w] and self.rect.y > 0:
             self.rect.y -= 5
         if keys_pressed[K_s] and self.rect.y < 500: 
             self.rect.y += 5   
     def update2(self):
-        keys_pressed = key.get_pressed()
-        if keys_pressed[K_LEFT] and self.rect.x > 0:
-            self.rect.x -= 5    
-        if keys_pressed[K_RIGHT] and self.rect.x < 950:
-            self.rect.x += 5
+        keys_pressed = key.get_pressed()   
         if keys_pressed[K_UP] and self.rect.y > 0:
             self.rect.y -= 5
         if keys_pressed[K_DOWN] and self.rect.y < 500:
@@ -57,8 +49,9 @@ RAKETKA2 = qwerSprite('raketka.png', 900, 300, 2)
 class Ball(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (100, 100))
-        self.speed = player_speed
+        self.image = transform.scale(image.load(player_image), (120, 120))
+        self.speed_x = player_speed
+        self.speed_y = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
@@ -69,13 +62,13 @@ class Ball(sprite.Sprite):
         pass
 
 RAKETKA = qwerSprite('raketka.png', 20, 300, 2)
-RAKETKA2 = qwerSprite('raketka.png', 900, 300, 2)
-BALL = Ball('boll.png', 50, 50, 2)
+RAKETKA2 = qwerSprite('raketka.png', 930, 300, 2)
+BALL = Ball('boll.png', 50, 50, 5)
 
 game = True
 finish = True
-
-
+q = 0
+player1 = 0
 
 while game:
     window.blit(background, (0, 0))
@@ -83,16 +76,44 @@ while game:
         if i.type == QUIT:
                 game = False
     if finish: 
+        if sprite.collide_rect(RAKETKA, BALL):
+            if q == 0:
+                q = 1
+            if q >= 1:
+                q += 1
+            if q == 14:
+                BALL.speed_x *= -1
+                q = 0
+        if sprite.collide_rect(RAKETKA2, BALL):
+            if q == 0:
+                q = 1
+            if q >= 1:
+                q += 1
+            if q == 8:
+                BALL.speed_x *= -1
+                q = 0
         RAKETKA.update()
         RAKETKA2.update2()
         RAKETKA.reset()
         RAKETKA2.reset()
+        BALL.rect.x += BALL.speed_x
+        BALL.rect.y += BALL.speed_y
+        if BALL.rect.y > 610 or BALL.rect.y < 0:
+            BALL.speed_y *= -1
         BALL.update()
         BALL.reset()
+        if BALL.rect.x > 950:
+            finish = False
+        if BALL.rect.x < 0:
+            player1 = 1
+            finish = False
     else:
-        text_win = font2.render('Победил 1 игрок!!!', 1, (230, 191, 0))
-        window.blit(text_win,(400, 300))
-        text_win2 = font2.render('Победил 2 игрок!!!', 1, (230, 191, 0))
-        window.blit(text_win,(400, 300))
+        window.blit(background, (0, 0))
+        if player1 == 1:
+            text_win = font2.render('Победил 1 игрок!!!', 1, (255, 255, 255))
+            window.blit(text_win,(400, 300))
+        else:
+            text_win2 = font2.render('Победил 2 игрок!!!', 1, (255, 255, 255))
+            window.blit(text_win2,(300, 300))
     display.update()
     clock.tick(FPS)
